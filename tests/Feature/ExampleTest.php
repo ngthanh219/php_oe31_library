774e4be9f35266b469653cdbec57f9e2dd7b9e25
Chanh_Ubuntu2020 <quangsoi99@gmail.com>
2020-12-31 14:46:21 +0700

    Setup Bower And Localization

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,10 +12,4 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
 }
