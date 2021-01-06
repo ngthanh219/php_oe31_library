d862465717c3c7ca8076ace460a02ef682be0322
Nguyen Thanh <nguyentienthanh9291@gmail.com>
2021-01-06 09:49:24 +0700

    Login/Logout

---
+++ resources/lang/en/login.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'hello' => 'Hello you',
+    'please_login' => 'Please login information',
+    'email' => 'Email',
+    'password' => 'Password',
+    'remember' => 'Remember me',
+    'forget_password' => 'Forget your password?',
+    'login' => 'Login',
+];
---
+++ resources/lang/vi/login.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'hello' => 'Chào bạn',
+    'please_login' => 'Mời bạn đăng nhập thông tin',
+    'email' => 'Email',
+    'password' => 'Mật khẩu',
+    'remember' => 'Ghi nhớ tài khoản',
+    'forget_password' => 'Quên mật khẩu?',
+    'login' => 'Đăng nhập',
+];
